6d089b16a653abec0472f5d9cef96a71034b2295
yavuzselimakyz <127423873+yavuzselimakyz@users.noreply.github.com>
2025-04-12 23:04:41 +0300

    update

--- Assets/GAME/PlatformMOve.cs
+++ Assets/GAME/PlatformMOve.cs
@@ -4,20 +4,25 @@ public class PlatformMove : MonoBehaviour
 {
     public Transform target; // Hedef obje
     public float speed = 2f; // Platform hýzý
-    private bool moving = true; // Hareket kontrolü
+    public bool moving = false; // Hareket kontrolü
 
-    public void Update()
+     void Update()
     {
         if (moving && target != null)
         {
             // Hedefe doðru hareket et
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-
-            // Hedefe ulaþtý mý?
-            if (Vector2.Distance(transform.position, target.position) < 0.1f)
+            if (moving == true) 
             {
-                moving = false; // Hareketi durdur
+
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+                // Hedefe ulaþtý mý?
+                if (Vector2.Distance(transform.position, target.position) < 0.1f)
+                {
+                    moving = false; // Hareketi durdur
+                }
             }
+
         }
     }
 }
--- Assets/GAME/kunaiTrigger.cs
+++ Assets/GAME/kunaiTrigger.cs
@@ -4,14 +4,20 @@ public class kunaiTrigger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     PlatformMove PlatformMove;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void Start()
     {
-        if (collision != null)
+        
+        PlatformMove = GetComponent<PlatformMove>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision != null)
         {
             if(collision.gameObject.tag == "Kunai")
             {
-                PlatformMove.Update();   
+                PlatformMove.moving = true;
             }
         }
     }
+ 
 }
